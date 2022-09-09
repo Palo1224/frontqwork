e8bb11ec0115f356fb9fe8ccccb6b0072d44aea5
PalomaV <palomavaira@hotmail.com>
2022-09-09 14:28:57 -0300

    cambios

--- src/components/Navbar/Contactenos/Contactenos.jsx
+++ src/components/Navbar/Contactenos/Contactenos.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./contactenos.module.scss";
 export const Contactenos = () => {
   return (
-    <div>Contactenos</div>
-  )
-}
+    <div className={styles.container}>
+        <h2> Sobre Nosotros</h2>
+        <p>
+          Gracias por visitarnos Si Usted esta interesado por nuestros servicios
+          o productos no dude en contactarnos completando este formulario.
+          Nuestros asistentes comerciales en breve estarán asesorandolo y
+          respondiendo todas sus inquietudes.
+        </p>
+      <div className={styles.form}>
+        <form>
+          <input></input>
+          <input></input>
+          <input></input>
+          <input></input>
+
+          <button>Enviar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
